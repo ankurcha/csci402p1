c68f89db109628810e7a67091298ac6585a0fb68
max <max@apollo.(none)>
2009-09-20 17:19:16 -0700

    reordered code

--- threads/threadtest.cc
+++ threads/threadtest.cc
@@ -89,12 +89,12 @@ void Problem2(int choice = -1)
                 HospINIT(3);
                 return;
                 break;
-            case 10:
-                HospINIT(10);
-                return;
-                break;
             case 4:
                 test4();
+                return;
+                break;
+            case 10:
+                HospINIT(10);
                 return;
                 break;
             default:
