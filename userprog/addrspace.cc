3f7662a3f06fd26a91edf1ae8c56b4e939f75d75
Ankur Chauhan <ankurcha@Ankur-Chauhans-MacBook-Pro.local>
2009-09-30 01:25:14 -0700

    Marked place for additions

--- userprog/addrspace.cc
+++ userprog/addrspace.cc
@@ -147,8 +147,10 @@ AddrSpace::AddrSpace(OpenFile *executable) : fileTable(MaxOpenFiles) {
 // first, set up the translation 
     pageTable = new TranslationEntry[numPages];
     for (i = 0; i < numPages; i++) {
-	pageTable[i].virtualPage = i;	// for now, virtual page # = phys page #
+            // TODO: @ankurcha: Get the TLB to do the translation for multi-programming
+	pageTable[i].virtualPage = i;	// for now, virtual page # = phys page # 
 	pageTable[i].physicalPage = i;
+            // @ankurcha
 	pageTable[i].valid = TRUE;
 	pageTable[i].use = FALSE;
 	pageTable[i].dirty = FALSE;
