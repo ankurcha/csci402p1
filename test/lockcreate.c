45b5935858ba22c533636218fc11732ed3d537ae
Ankur Chauhan <ankurcha@usc.edu>
2009-10-10 15:52:07 -0700

    bugfixes

--- test/lockcreate.c
+++ test/lockcreate.c
@@ -1,17 +1,21 @@
 #include "syscall.h"
 
+char* itoa(int a){
+    static char str[50];
+    int i = 49;
+    do{
+        str[i] = '0'+ a%10;
+    }while((a=a/10) && i>=0);
 
-main() {
-
-int w = CreateLock("lock_test");
-Acquire(w);
-Release(w);
-if(w>=0){
-	Write(" Testing lock creation...Pass\n" , 50, ConsoleOutput );
- }
-else {
-	Write(" Testing lock creation...Fail\n" , 50, ConsoleOutput );
+ return str;
 }
 
-
-       }
+int main() {
+    int w = CreateLock("lockTest");
+    Write(itoa(0),1,1);
+    if(w>=0){
+        Write("Pass2\n",6,1);
+    }else{
+        Write("Fail3\n",6,1);
+    }
+}
