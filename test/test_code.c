07151215e136ca2b3fa7d5af875980f2f21d54cf
Ankur Chauhan <ankurcha@usc.edu>
2009-10-11 13:01:49 -0700

    Test_code

--- test/test_code.c
+++ test/test_code.c
@@ -5,8 +5,8 @@ LockId lockId;/*Integer type Lock Id*/
 CVId conditionId; /* Integer type condition Id*/
 void test1();
 void test1_t();
-void p2test2();
-void p2test2_thread2();
+void test2();
+void test2_thread2();
 void p2test2_thread3();
 void p2test3();
 void p2test3_thread2();
@@ -21,8 +21,8 @@ int main()
     test1();
     test1_t();
     
-    p2test2();
-    p2test2_thread2();
+    test2();
+    test2_thread2();
     p2test2_thread3();
     
     p2test3();
@@ -39,17 +39,14 @@ void test1()
 	lockId= CreateLock("t1_l1");                                             
     /* Lock is Created */
 	Fork(test1_t);                                               
-    /* Fork to create second thread */
-	print("Thread 1 acquiring lock\n" );              
-    /* printout for acquiring a lock */
-	Acquire(lockId);                                                   
+    /* Fork to create second thread */              
+	Acquire(lockId);                  
     /* Calling a Acquire function */
 	print("Thread 1 lock acquired\n" );               
-    /* Printout that lock is Acquired */
-	print("Thread 1 lock released\n" );               
-    /* Printout that lock is released*/
+    /* Printout that lock is Acquired */              
 	Release(lockId);                                                   
     /* Calling a Release function*/
+    print("Thread 1 lock released\n" );
 }
 
 void test1_t()
@@ -57,17 +54,16 @@ void test1_t()
 	Acquire(lockId);                                                   
     /* Calling a acquire function*/
 	print("thread 2 lock acquired\n" );               
-    /* Printout for acquiring a lock*/
-	print("thread 2 lock released\n" );               
     /* Printout for releasing a lock*/
 	Release(lockId);
+    print("thread 2 lock released\n" );               
 }
 
-void p2test2()
+void test2()
 {
-	Fork(p2test2_thread2);                                                
+	Fork(test2_thread2);                                                
     /* Fork to create second thread*/
-	Fork(p2test2_thread3);                                               
+	Fork(test2_thread3);                                               
     /* Fork to create third thread */
 	print("thread 1 acquiring lock\n" );              
     /* Printout for acquiring a lock*/
@@ -80,7 +76,7 @@ void p2test2()
 	print("thread 1 lock released\n" );
 }
 
-void p2test2_thread2()
+void test2_thread2()
 {
 	Acquire(lockId);                                                  
     /* Calling a Acquire Function*/                        
@@ -92,33 +88,37 @@ void p2test2_thread2()
     /* Printout that lock is destroyed */
 }
 
-void p2test2_thread3()
+void test2_thread3()
 {	
+    int i=50;
 	lockId= CreateLock("t2_l2");                                                 
     /* Lock is created*/
 	print("thread 3 acquiring lock\n" );                  
     /* Printout for acquiring a lock*/
+    for (i=50; i>0; i--) {
+        Yield();
+    }
 	Release(lockId);                                                      
     /* Lock is released */
 	print("thread 3 releasing lock\n" );                  
     /* Printout that lock is released*/
 }	
 
-void p2test3()
+void test3()
 {
 	conditionId= CreateCondition("t3_c1");                                                                   
     /* Condition variable is created */
 	print("thread 1  creating condition variable\n" );                                
     /* Printout for creating a condition variable*/
-	lockId= CreateLock("t1_l1");                                                                             
+	lockId= CreateLock("t3_l1");                                                                             
     /* Lock is created */
 	print("thread 1 acquiring lock\n" );                                              
     /* Printout for acquiring a lock*/
-	Fork(p2test3_thread2);                                                                             
+	Fork(test3_thread2);                                                                             
     /* Fork to create second thread*/
-	Fork(p2test3_thread3);                                                                             
+	Fork(test3_thread3);                                                                             
     /* Fork to create third thread */
-	Fork(p2test3_thread4);                                                                             
+	Fork(test3_thread4);                                                                             
     /* Fork to create fourth thread */
 	Wait(conditionId, lockId);                                                                       
     /* Wait signal is called*/
@@ -134,9 +134,9 @@ void p2test3()
     /* Printout that on broadcasting*/ 
 }
 
-void p2test3_thread2()
+void test3_thread2()
 {
-	lockId= CreateLock("t1_l2");                                                          
+	lockId= CreateLock("t3_l2");                                                          
     /* Lock is created */
 	print("thread 2 creating a lock\n" );                
 	Wait(conditionId, lockId);                                                    
@@ -152,9 +152,9 @@ void p2test3_thread2()
     /* Destroying a condition variable */
 }
 
-void p2test3_thread3()
+void test3_thread3()
 {
-	conditionId= CreateCondition("t1_c2");                                       
+	conditionId= CreateCondition("t3_c2");                                       
     /* Condition variable is created */
 	print("thread 3 creating a condition variable\n"); 
 	Signal(conditionId, lockId);                                         
@@ -165,7 +165,7 @@ void p2test3_thread3()
 	print("thread 3 broadcasting \n");
 }
 
-void p2test3_thread4()
+void test3_thread4()
 {
 	
 	Wait(conditionId, lockId);                          
