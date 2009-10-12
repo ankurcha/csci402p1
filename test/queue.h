73051bdfe3dfae318b61fcda7d6007dd325c9662
max <max@apollo.(none)>
2009-10-12 16:53:49 -0700

    added library for the queue data structure

---
+++ test/queue.h
@@ -0,0 +1,34 @@
+/* library to provide a queue data structure with no dynamic memory
+ *
+ * CS 402 Fall 2009
+ * Max Pflueger
+ */
+
+#ifndef QUEUE_H
+#define QUEUE_H
+
+struct queue_element{
+    int value;
+    int next = -1;
+    char valid = 0;
+};
+
+struct Queue{
+    queue_element* queue;
+    int length;
+    int head;
+    int tail;
+}
+
+/* methods for a FIFO queue */
+
+/* Push a value to the end of the queue 
+ *  Note that the first arg must be an array of length n*/
+void Queue_Push(queue_element* queue, int length, int* head, int* tail, int value);
+void Queue_Push(Queue* queue, int value);
+
+/* Pop a value off the top of the queue */
+int Queue_Pop(queue_element* queue, int length, int* head, int* tail);
+int Queue_Pop(Queue* queue);
+
+#endif /* QUEUE_H */
