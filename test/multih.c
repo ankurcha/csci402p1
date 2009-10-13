186fa0e212bad4972d32460aaf2944aa71b6f177
Ankur Chauhan <ankurcha@usc.edu>
2009-10-13 03:37:42 -0700

    multi hospital

---
+++ test/multih.c
@@ -0,0 +1,8 @@
+#include "syscall.h"
+#include "print.c"
+int main(){
+print("Spawning 3 Hospital simulations\n");
+Exec("/home/scf-10/ankurcha/code/test/init");
+Exec("/home/scf-10/ankurcha/code/test/init");
+Exec("/home/scf-10/ankurcha/code/test/init");
+}
