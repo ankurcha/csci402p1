b9c2126513b8602d96c4e9f7be56fb03cde94f07
pflueger <pflueger@aludra.usc.edu>
2009-10-12 22:37:10 -0700

    lots more debugging, and some locks and cvs

--- test/fork.c
+++ test/fork.c
@@ -1,13 +1,38 @@
+/* This program tests the functionality of our Fork call, 
+ *  by assuring that func1 is forked and run
+ */
+
 #include "syscall.h"
 #include "print.c"
 
+int lock, cv;
+
 void func1(){
     print("func1 forked and running\n");
+
+    Acquire(lock);
+    print("func1 acquired lock\n");
+    Signal(cv, lock);
+
+    print("func1 Exiting with exit status 0\n");
+
+    Release(lock);
     Exit(0);
 }
 
 void main(){
+    lock = CreateLock("lock");
+    cv = CreateCondition("condition");
+    Acquire(lock);
+
     Fork(func1);
+
+    print("main forked func1\n");
+    Wait(cv, lock);
+    print("main got signaled\n");
+    Release(lock);
+    print("main released lock\n");
     Yield();
     Exit(0);
 }
+
