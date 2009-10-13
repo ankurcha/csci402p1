c673b3ccc05fbc6963a1eaf5b50b02cdcd3d9ad1
max <max@apollo.(none)>
2009-10-12 23:50:42 -0700

    fixed itoa to omit leading 0's

--- test/itoa.c
+++ test/itoa.c
@@ -3,14 +3,28 @@
  *  
  *
  *  Created by Ankur Chauhan on 10/10/09.
- *  Copyright 2009 University of Southern California. All rights reserved.
  *
  */
 
 char* itoa(int a, char *str){
-    int i = 49;
+    int i = 0;
+    int j = 0;
+    char rts[50];
+
+    // print the string in reverse
     do{
-        str[i] = '0'+ a%10;
-    }while((a=a/10) && i>=0);
+        rts[i] = '0'+ a%10;
+        i++;
+    }while((a=a/10) && i < 49);
+    i--;
+
+    // reverse it into the output, now it is forward
+    while(i >= 0 && j < 32) {
+        str[j] = rts[i];
+        i--;
+        j++;
+    }
+    str[j] = 0;
+
     return str;
 }
