2ad5c4938ae50ca07f4fa2fd3483721571f85fcf
max <max@apollo.(none)>
2009-10-13 19:30:05 -0700

    fixed indentation

--- test/multih.c
+++ test/multih.c
@@ -1,12 +1,14 @@
 #include "syscall.h"
 #include "print.c"
+
 int main(){
-print("Spawning 3 Hospital simulations\n");
-/*Exec("/home/scf-10/ankurcha/code/test/init");
-Exec("/home/scf-10/ankurcha/code/test/init");
-Exec("/home/scf-10/ankurcha/code/test/init");
-*/
-Exec("../test/init");
-Exec("../test/init");
-Exec("../test/init");
+    print("Spawning 3 Hospital simulations\n");
+    /*Exec("/home/scf-10/ankurcha/code/test/init");
+    Exec("/home/scf-10/ankurcha/code/test/init");
+    Exec("/home/scf-10/ankurcha/code/test/init");
+    */
+    Exec("../test/init");
+    Exec("../test/init");
+    Exec("../test/init");
 }
+
