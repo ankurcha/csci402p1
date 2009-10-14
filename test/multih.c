9eeb10d79cfb5a52022640c3c88db38842ee3186
Ankur Chauhan <ankurcha@usc.edu>
2009-10-14 00:14:17 -0700

    Final Submitted version of Project 2

--- test/multih.c
+++ test/multih.c
@@ -3,10 +3,6 @@
 
 int main(){
     print("Spawning 3 Hospital simulations\n");
-    /*Exec("/home/scf-10/ankurcha/code/test/init");
-    Exec("/home/scf-10/ankurcha/code/test/init");
-    Exec("/home/scf-10/ankurcha/code/test/init");
-    */
     Exec("../test/init");
     Exec("../test/init");
     Exec("../test/init");
