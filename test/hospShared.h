fea2992472c21873ae01557d7b2d9f19acaa15c5
Ankur Chauhan <ankurcha@guest-wireless-207-151-228-166.usc.edu>
2009-11-23 15:09:09 -0800

    bugfixes

--- test/hospShared.h
+++ test/hospShared.h
@@ -9,17 +9,12 @@
 #define HOSPSHARED_H_
 
 /* Data Update packet building functions */
-Packet* buildPacket_Receptionist(Packet *p, int id, int peopleInLine,
-        int currentToken);
-Packet *buildPacket_Doctor(Packet *p, int id, int peopleInLine,
-        int prescription, int patientToken);
-Packet *buildPacket_Cashier(Packet *p, int id, int lineLength, int patToken,
-        int fee, int payment, int sales);
-Packet *buildPacket_Clerk(Packet *p, int id, int patientsInLine, int payment,
-        int fee, int patPrescription, int sales);
+Packet* buildPacket_Receptionist(Packet *p, int id);
+Packet *buildPacket_Doctor(Packet *p, int id);
+Packet *buildPacket_Cashier(Packet *p, int id);
+Packet *buildPacket_Clerk(Packet *p, int id);
 Packet *buildPacket_GlobalData(Packet *p, short variable, int val);
-Packet *buildPacket_GlobalListAppend(Packet *p, short variable, int key,
-        int val);
+Packet *buildPacket_GlobalListAppend(Packet *p, int key, int val);
 Packet *buildPacket_GlobalQueuePush(Packet *p, int val);
 Packet *buildPacket_GlobalQueuePop(Packet *p);
 #endif /* HOSPSHARED_H_ */
