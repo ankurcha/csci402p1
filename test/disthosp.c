8416fe280a55232a00536e5d6bd6c91b3d43f115
max <max@apollo.(none)>
2009-11-23 12:50:17 -0800

    finished the hospital entity spawning program

--- test/disthosp.c
+++ test/disthosp.c
@@ -8,50 +8,80 @@
  */
 
 #include "p2pnetwork.h"
+#include "packet.h"
 
 int main() {
     int machineID = 0;
     int threadCt = 0;
     int i = 0;
+    char buf[50];
+    char message[MaxMailSize];
 
     machineID = GetMachineID();
 
-    /*TODO determine threadCt by reading conf file */
+    /* determine threadCt by reading conf file */
+    readConfig();
+    threadCt = numberOfEntities[machineID];
 
-    /*TODO put msgs into mailbox 0 with mail box numbers */
+    /* put msgs into mailbox 0 with mail box numbers */
+    for(i=0; i < threadCt; i++) {
+        copyInInt(message, 0, i+1);
+        Send(machineID, 0, 0, message);
+    }
 
     switch (machineID) {
         case 0: /* patients */
+            print("disthosp: preparing to create ");
+            print(itoa(threadCt, buf));
+            print(" patient entities.\n");
             for (i = 0; i < threadCt; i++) {
                 Exec("../test/patient");
             }
             break;
         case 1: /* receptionists */
+            print("disthosp: preparing to create ");
+            print(itoa(threadCt, buf));
+            print(" receptionist entities.\n");
             for (i = 0; i < threadCt; i++) {
                 Exec("../test/receptionist");
             }
             break;
         case 2: /* doorboys */
+            print("disthosp: preparing to create ");
+            print(itoa(threadCt, buf));
+            print(" doorboy entities.\n");
             for (i = 0; i < threadCt; i++) {
                 Exec("../test/doorboy");
             }
             break;
         case 3: /* doctors */
+            print("disthosp: preparing to create ");
+            print(itoa(threadCt, buf));
+            print(" doctor entities.\n");
             for (i = 0; i < threadCt; i++) {
                 Exec("../test/doctor");
             }
             break;
         case 4: /* cashiers */
+            print("disthosp: preparing to create ");
+            print(itoa(threadCt, buf));
+            print(" cashier entities.\n");
             for (i = 0; i < threadCt; i++) {
                 Exec("../test/cashier");
             }
             break;
         case 5: /* clerks */
+            print("disthosp: preparing to create ");
+            print(itoa(threadCt, buf));
+            print(" clerk entities.\n");
             for (i = 0; i < threadCt; i++) {
                 Exec("../test/clerk");
             }
             break;
         case 6: /* hospital manager */
+            print("disthosp: preparing to create ");
+            print(itoa(threadCt, buf));
+            print(" hospManager entity.\n");
             for (i = 0; i < threadCt; i++) {
                 Exec("../test/hospManager");
             }
