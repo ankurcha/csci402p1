fd9707f94941e282eb7d1e9d0cdf9c70d3a27528
max <max@apollo.(none)>
2009-11-22 22:09:08 -0800

    executable to spawn hospital entities

---
+++ test/disthosp.c
@@ -0,0 +1,20 @@
+/* Main function to execute the distributed hospital
+ *  This function will spawn the required number of processes
+ *  of the entity type specified by the machine id
+ *
+ * USC CS 402 Fall 2009
+ * Group 11
+ * Max Pflueger
+ */
+
+#include "cash.c"
+#include "clerk.c"
+#include "doctor.c"
+#include "doorboy.c"
+#include "hospManager.c"
+#include "patient.c"
+#include "receptionist.c"
+
+int main() {
+}
+
