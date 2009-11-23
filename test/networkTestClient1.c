cf54a47a14c54aa01fcc6020972bf7189665d94e
pflueger <pflueger@aludra.usc.edu>
2009-11-22 21:37:37 -0800

    matched to new syscall prototypes

--- test/networkTestClient1.c
+++ test/networkTestClient1.c
@@ -8,22 +8,23 @@ int main() {
 	 * 1 on mailbox 0 and then displays it
 	 */
 
+        int senderId, senderMbox;
 	int response;
 	char *message = "foo";
 	char *receivedMessage;
 	print("Client 1: Sending foo\n");
-	Send(1, 0, message);
+	Send(1, 0, 0, message);
 
-	response = Receive(0, 0, receivedMessage);
+	response = Receive(0, &senderId, &senderMbox, receivedMessage);
 
 	print("Client 1: ");
 	print(receivedMessage);
 	print("\n");
 
 	print("Trying to send to a bad receiver = -1\n");
-	Send(-1, 0, message);
+	Send(-1, 0, 0, message);
 	print("\n Trying to receive from bad receiver = -1\n");
-	response = Receive(-1, 0, receivedMessage);
+	response = Receive(-1, &senderId, &senderMbox, receivedMessage);
 	print("Client 1: ");
 	print(receivedMessage);
 	print("\n");
