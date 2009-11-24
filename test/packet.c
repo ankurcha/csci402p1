058461930ef34bdd2ce431d1ac4837cad068c7f0
max <max@apollo.(none)>
2009-11-23 21:25:57 -0800

    declared i

--- test/packet.c
+++ test/packet.c
@@ -141,6 +141,7 @@ void DeserializePacket(Packet *p, char* message) {
 
 /* deep copy memory y to x */
 void memcopy(void* x, void* y, int length) {
+    int i=0;
     for(i=0; i < length; i++) {
         *(x+i) = *(y+i);
     }
