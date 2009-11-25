524a9c36e3cf61d2ec3e0015bea50b5cf1b01e40
Ankur Chauhan <ankurcha@usc.edu>
2009-11-24 17:52:04 -0800

    Increased the number of mailboxes

--- threads/system.cc
+++ threads/system.cc
@@ -178,7 +178,7 @@ void Initialize(int argc, char **argv) {
 #endif
 
 #ifdef NETWORK
-    postOffice = new PostOffice(netname, rely, 10);
+    postOffice = new PostOffice(netname, rely, 100);
 #endif
 }
 
